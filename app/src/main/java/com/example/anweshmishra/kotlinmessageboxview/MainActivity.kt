afc399998f866db1b89120df31cdeae2841b4321
Anwesh43 <anweshthecool0@gmail.com>
2018-05-09 03:11:06 +0530

    addding fullScreen support

--- app/src/main/java/com/example/anweshmishra/kotlinmessageboxview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinmessageboxview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinmessageboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.messageboxview.MessageBoxView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MessageBoxView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
