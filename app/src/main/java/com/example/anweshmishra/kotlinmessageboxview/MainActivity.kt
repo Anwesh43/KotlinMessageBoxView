0c2fa7ee55eda8aea90e297ecea589ae7d7c6475
Anwesh43 <anweshthecool0@gmail.com>
2018-05-09 03:08:02 +0530

    adddding view in activity

--- app/src/main/java/com/example/anweshmishra/kotlinmessageboxview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinmessageboxview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinmessageboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.messageboxview.MessageBoxView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MessageBoxView.create(this)
     }
 }
