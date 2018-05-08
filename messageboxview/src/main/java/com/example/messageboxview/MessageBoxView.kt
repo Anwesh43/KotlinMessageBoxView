76b65fea960e31e163c2ae5d38ca4d040c4dbe7e
Anwesh43 <anweshthecool0@gmail.com>
2018-05-09 02:41:53 +0530

    addding MessageBox class

--- messageboxview/src/main/java/com/example/messageboxview/MessageBoxView.kt
+++ messageboxview/src/main/java/com/example/messageboxview/MessageBoxView.kt
@@ -78,4 +78,65 @@ class MessageBoxView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MessageBox (var i : Int, val state : State = State()) {
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val r : Float = Math.min(w, h)/10
+            val size : Float = Math.min(w, h)/3
+            canvas.save()
+            canvas.translate(w/2, h/2)
+            paint.color = Color.WHITE
+            canvas.drawTail(-r, r, state.scales[0], paint)
+            canvas.drawBox(-r, size, state.scales[1], paint)
+            paint.color = Color.BLACK
+            canvas.drawMultipleLines(-r - size, 0.8f * size, size, 3, state.scales[2], paint)
+            paint.color = Color.WHITE
+            canvas.drawCircle(0f, 0f, r, paint)
+            paint.color = Color.BLACK
+            canvas.drawLine(0f, -r/2 * state.scales[2], 0f, r/2 * state.scales[2], paint)
+            canvas.restore()
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
+}
+
+fun Canvas.drawTail(y:Float, size : Float,  scale : Float, paint : Paint) {
+    save()
+    translate(0f, -y * scale)
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(-size/2, -size)
+    path.lineTo(size/2, -size)
+    drawPath(path, paint)
+    restore()
+}
+
+fun Canvas.drawBox(y : Float, size : Float, scale : Float, paint : Paint) {
+    drawRect(RectF(-size/2, y - size * scale, size/2, y), paint)
+}
+
+fun Canvas.drawMultipleLines(yStart : Float, size : Float, h : Float,  n : Int, scale : Float paint : Paint) {
+    paint.color = Color.BLACK
+    paint.strokeWidth = size/20
+    paint.strokeCap = Paint.Cap.ROUND
+    val gap : Float = h / (2 * n + 1)
+    val w : Float = (size / 2 ) * scale
+    save()
+    translate(yStart, 0f)
+    var y : Float = gap
+    for (i in 1..n) {
+        drawLine(-w, 0f, w, 0f, paint)
+        y += gap
+    }
+    restore()
 }
